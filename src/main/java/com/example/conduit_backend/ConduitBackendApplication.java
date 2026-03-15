e719d841b9637a191d29439193e3a477cdfd5551
Rakesh173 <gurusamyrenuga4@gmail.com>
2026-03-15 12:42:12 +0530

    Initial commit

---
+++ src/main/java/com/example/conduit_backend/ConduitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.conduit_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConduitBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConduitBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/conduit_backend/ConduitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.conduit_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConduitBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
